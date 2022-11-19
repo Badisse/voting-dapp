128917e8d02d8d679e1f2c2b31cabd4eb19eee58
Badisse Bouabdallah <badisse.bouabdallah@proton.me>
2022-11-19 03:52:24 +0100

    test precommit

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,8 +4,6 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-const test = 'rfsw'
-
 const root = ReactDOM.createRoot(document.getElementById('root') as Element)
 root.render(
   <React.StrictMode>
